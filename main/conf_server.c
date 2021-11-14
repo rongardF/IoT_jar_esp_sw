19b4d0bccf32568c5fb12821d1bf72a6a27ad712
Ron Freimann <ronfreimann@gmail.com>
2021-11-14 16:20:31 +0200

    Basic functionality of receiving network details over WiFi

--- main/conf_server.c
+++ main/conf_server.c
@@ -13,16 +13,86 @@
 #include "conf_server.h"
 #include "wifi_iot.h"
 
-void conf_server()
-{
+struct netdata {
+	uint8_t SSID[32];
+	uint8_t PASSWORD[64];
+};
 
+void conf_server(int32_t socket)
+{
+	uint32_t payload_len, word_len;
+	uint8_t recv_buffer[100] = {0};
+	struct netdata new_conf;
+
+	if (recv(socket, &payload_len, sizeof(payload_len), 0) < 0)
+	{
+		ESP_LOGE("CONF_SERVER", "Failed to receive payload size value\r\n");
+	}
+
+	payload_len = ntohl(payload_len);
+
+	ESP_LOGI("CONF_SERVER", "Payload size %u\r\n",payload_len);
+
+	if (recv(socket, &word_len, sizeof(word_len), 0) < 0)
+	{
+		ESP_LOGE("CONF_SERVER", "Failed to receive word size value\r\n");
+	}
+
+	word_len = ntohl(word_len);
+
+	ESP_LOGI("CONF_SERVER", "Word size %u\r\n",word_len);
+
+	if (recv(socket, recv_buffer, word_len, 0) < 0)
+	{
+		ESP_LOGE("CONF_SERVER", "Failed to receive SSID word\r\n");
+	}
+
+	for (int i=0; i<=word_len; i++)
+	{
+		new_conf.SSID[i] = recv_buffer[i];  // this does not need to have nthol for endianess conversion because only single byte!
+		if (i == word_len)
+		{
+			new_conf.SSID[i] = 0;  //null character to signal the end of string (\0)
+		}
+	}
+
+	if (recv(socket, &word_len, sizeof(word_len), 0) < 0)
+	{
+		ESP_LOGE("CONF_SERVER", "Failed to receive word size value\r\n");
+	}
+
+	word_len = ntohl(word_len);
+
+	ESP_LOGI("CONF_SERVER", "Word size %u\r\n",word_len);
+
+	if (recv(socket, recv_buffer, word_len, 0) < 0)
+	{
+		ESP_LOGE("CONF_SERVER", "Failed to receive PASSWORD word\r\n");
+	}
+
+	for (int i=0; i <= word_len; i++)
+	{
+		new_conf.PASSWORD[i] = recv_buffer[i]; // this does not need to have nthol for endianess conversion because only single byte!
+		if (i == word_len)
+		{
+			new_conf.PASSWORD[i] = 0;  //null character to signal the end of string (\0)
+		}
+	}
+
+	ESP_LOGI("CONF_SERVER", "SSID: %s\r\n",(char*) new_conf.SSID);
+	ESP_LOGI("CONF_SERVER", "Password: %s\r\n",(char*) new_conf.PASSWORD);
+
+	while (1)
+	{
+		vTaskDelay(5000 / portTICK_RATE_MS);
+	}
 }
 
 // this creates a server which accepts connection from the user to configure the device
-void listener (){
+void listener ()
+{
 	struct sockaddr_in server_addr, client_addr;
 	int32_t sockfd, user_socket;
-	char message[200];
 
 	if (( sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
@@ -55,13 +125,13 @@ void listener (){
 		}
 
 		// create a task which communicates with client application to receive home network information and commands
-		xTaskCreate(conf_server, "conf_server", 1024, NULL, 5, NULL);
+		xTaskCreate(conf_server, "conf_server", 4096, user_socket, 5, NULL);
 
-		// wait until some event happens
+		// wait until some event happens - eventGroupWaitBits here
 
 		// check if STA disconnected or client app closed connection - wait for a new connection if configuration was not finished
 
-		// check if all configuration information received and have communication with MQTT broker
+		// check if all configuration information received and have communication with MQTT broker - delete both tasks then and re-configurefor STA only
 
 	}
 
